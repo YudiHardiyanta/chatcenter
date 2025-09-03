9f4c7ce82ab18e2a723affc48d803ad93b271f96
yudi.hardiyanta <yudi.hardiyanta@bps.go.id>
2025-09-03 11:00:52 +0800

    menambahkan mekanisme login logout

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const bcrypt = require('bcrypt');
+const session = require('express-session');
 
+// Middleware
+app.use(express.urlencoded({ extended: true }));
 
 // Konfigurasi view engine EJS
 app.set('view engine', 'ejs');
@@ -11,9 +15,35 @@ app.set('views', path.join(__dirname, 'views'));
 // Middleware untuk file statis (CSS, gambar, JS di sisi klien)
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({
+  secret: '68b79e18-d7d8-8332-92ed-5181de87c653', // ganti dengan yang lebih aman
+  resave: false,
+  saveUninitialized: false
+}));
+
+// Middleware cek login
+function isAuthenticated(req, res, next) {
+  if (req.session.user) return next();
+  res.redirect('/login');
+}
+
+
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+
+  const match = username=='adminpst51' && password=='uti@5100';
+
+  if (!match) {
+    return res.render('login', { error: 'Password salah' });
+  }
+
+  // Simpan user ke session
+  req.session.user = { username: username };
+  res.redirect('/');
+});
 
 // Routing
-app.get('/', (req, res) => {
+app.get('/',isAuthenticated, (req, res) => {
     var jwt = require("jsonwebtoken");
 
     var METABASE_SITE_URL = "https://metabase.statsbali.id";
@@ -31,9 +61,15 @@ app.get('/', (req, res) => {
     res.render('index', { title: 'Beranda', year: new Date().getFullYear() ,iframeUrl : iframeUrl});
 });
 
+app.get('/logout',isAuthenticated, (req, res) => {
+  req.session.destroy(() => {
+    res.redirect('/login');
+  });
+});
 
-app.get('/about', (req, res) => {
-    res.render('about', { title: 'Tentang', year: new Date().getFullYear() });
+
+app.get('/login', (req, res) => {
+    res.render('login', { title: 'Login', year: new Date().getFullYear() });
 });
 
 
